9a85a8b7eb2dcfbd79cfdfcce180b1e74e3e9f89
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-04-28 00:48:57 -0700

    sds

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
 
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application) 
