15e236a142ef0ae60856d31e96cb9b23f709d8f7
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-04-28 02:04:25 -0700

    sds

--- webapp/settings.py
+++ webapp/settings.py
@@ -30,8 +30,8 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'service.apps.ServiceConfig',
     'chat.apps.ChatConfig',
-    # 'channels',
-    'whitenoise.runserver_nostatic',
+    'channels',
+    # 'whitenoise.runserver_nostatic',
 
     # Django libraries
     'django.contrib.admin',
@@ -121,13 +121,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-
-
+# Media Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
