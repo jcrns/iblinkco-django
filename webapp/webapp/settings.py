8641700ec0fec2f974e8de5225cf3738da1c185a
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-04-27 22:15:30 -0700

    sds

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
