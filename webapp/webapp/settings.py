1c5968ebb55bd47ac08b90173ce8d45a92eed1c4
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-04-27 22:05:39 -0700

    sds

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -120,10 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-
-# STATICFILES = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
