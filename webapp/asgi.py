a44f010159e747ce957dc3514e068ce5a6c7e444
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-05-02 13:29:10 -0700

    Please work

--- webapp/asgi.py
+++ webapp/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import django
+from channels.routing import get_default_application
+import channels.asgi
 
-from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+application = get_default_application()
+channel_layer = channels.asgi.get_channel_layer()
