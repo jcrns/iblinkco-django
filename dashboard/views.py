e7a45925dd5729a4551d2bafdd98616f929e956f
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-05-06 17:05:46 -0700

    Added test

--- dashboard/views.py
+++ dashboard/views.py
@@ -448,7 +448,7 @@ def jobPrepEnded(request, pk):
 def test(request):
     if request.method == 'POST':
         print('sdsdsds')
+        testTask.apply_async(countdown=5)
         testTask.apply_async(countdown=100)
-        testTask.apply_async(countdown=1800)
         return redirect('dashboard-test')
     return render(request, 'dashboard/test.html')
