48d658d0fa31ccfed74fd6796e52a4efea513706
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-05-09 13:49:29 -0700

    Remove job offer class

--- management/models.py
+++ management/models.py
@@ -97,7 +97,3 @@ def manager_acceptance(sender, instance, **kwargs):
                 # Sending Email if user was accepted
                 ManagerEvaluation.managerAccepted(instance)
 
-
-class JobOffer(models.Model):
-    job = models.ForeignKey(JobPost, on_delete=models.CASCADE)
-    user = models.ForeignKey(JobPost, on_delete=models.CASCADE)
