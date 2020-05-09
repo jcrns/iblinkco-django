5a047729743d1794e80b204a62ffd73ea95eea20
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-05-09 06:59:53 -0700

    Fixed manager selection

--- webapp/urls.py
+++ webapp/urls.py
@@ -31,7 +31,5 @@ urlpatterns = [
     path('', include('chat.urls')),
 ]
 
-# manager_assignment()
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
