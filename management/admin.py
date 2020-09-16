6566c51e2042007f387c31939555aac2c2778670
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2020-09-16 00:03:59 -0700

    Adding manager preference model

--- management/admin.py
+++ management/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ManagerEvaluation, ManagerBiases, ManagerPreference
+from .models import ManagerEvaluation#, ManagerBiases, ManagerPreference
 
 
 class ManagerEvaluationAdmin(admin.ModelAdmin):
@@ -13,5 +13,5 @@ class ManagerEvaluationAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ManagerEvaluation, ManagerEvaluationAdmin)
-admin.site.register(ManagerBiases)
-admin.site.register(ManagerPreference)
+# admin.site.register(ManagerBiases)
+# admin.site.register(ManagerPreference)
